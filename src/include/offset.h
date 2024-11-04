dd25f76ec9326c90f7650b68cb9b71a5e9d2088d
uttamkn <uttamkn15@gmail.com>
2024-11-04 11:57:44 +0530

    add offset header file to define all offsets

---
+++ src/include/offset.h
@@ -0,0 +1,12 @@
+#ifndef OFFSET_H
+#define OFFSET_H
+
+#define HEADER_SIZE 100
+
+#define PAGE_SIZE_OFFSET 16
+
+#define NO_OF_CELLS_OFFSET 3
+
+#define DEFAULT_PAGE_SIZE 4096
+
+#endif // OFFSET_H
--- src/lib/dbinfo.c
+++ src/lib/dbinfo.c
@@ -1,9 +1,10 @@
 #include "../include/dbinfo.h"
+#include "../include/offset.h"
 #include "../include/utils.h"
 
 unsigned short getPageSize(FILE *database_file) {
   unsigned char buffer[2];
-  if (readBytes(database_file, 16, 2, buffer) == 0) {
+  if (readBytes(database_file, PAGE_SIZE_OFFSET, 2, buffer) == 0) {
     return convertBytesToShort(buffer);
   }
 
@@ -11,9 +12,13 @@ unsigned short getPageSize(FILE *database_file) {
   return -1;
 }
 
-unsigned short getNoOfCellsOfRootPage(FILE *database_file) {
+unsigned short getNoOfCells(FILE *database_file, int page_no) {
   unsigned char buffer[2];
-  if (readBytes(database_file, 103, 2, buffer) == 0) {
+
+  int current_page_offset = HEADER_SIZE + DEFAULT_PAGE_SIZE * (page_no - 1);
+
+  if (readBytes(database_file, current_page_offset + NO_OF_CELLS_OFFSET, 2,
+                buffer) == 0) {
     return convertBytesToShort(buffer);
   }
 
@@ -21,18 +26,8 @@ unsigned short getNoOfCellsOfRootPage(FILE *database_file) {
   return -1;
 }
 
-unsigned int _getNoOfPages(FILE *database_file) {
-  unsigned char buffer[4];
-  if (readBytes(database_file, 28, 4, buffer) == 0) {
-    return convertBytesToInt(buffer);
-  }
-
-  perror("Failed to read the number of pages");
-  return -1;
-}
-
 unsigned int getNoOfTables(FILE *database_file) {
   // This is temporary (doesnt work if the tables are stored in more than one
   // page)
-  return getNoOfCellsOfRootPage(database_file);
+  return getNoOfCells(database_file, 1);
 }
